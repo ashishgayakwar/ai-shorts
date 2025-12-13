04e3c6e2e9c710eb43ba7d35a982674c4d799113
Ashish Gayakwar <ashishgayakwar@Ashishs-MacBook-Pro-2.local>
2025-12-13 16:47:40 +0530

    corected sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: "/",
       },
     ],
-    sitemap: "https://ashishgayakwar.com/sitemap.xml",
+    sitemap: "https://www.aipmworld.com/sitemap.xml",
   };
 }
